e030e9897cd21da989a364ba2d29b7fbb26d8754
Vanessa Schenkel <v.s.schenkel@gmail.com>
2022-03-15 16:12:17 -0300

    fix cors

--- api/server.py
+++ api/server.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, abort
-from flask_cors import CORS
+from flask import Flask, jsonify, request, json
+from flask_cors import CORS, cross_origin
 from googletrans import Translator
 
 app = Flask(__name__)
-CORS(app, resources=r'*')
+CORS(app)
+
 
 textToTranslate = ""
 textTranslated = ""
@@ -12,17 +13,26 @@ translator = Translator()
 def translation(textToTranslate):
     return translator.translate(textToTranslate, src="en", dest="pt").text
 
-@app.route("/")
-def helloWorld():
-  return "Hello, cross-origin-world!"
 
-@app.route("/create", methods=['POST'])
+@app.route("/", methods=["GET", "OPTIONS"])
+@cross_origin()
+def helloWorld():
+    response = jsonify(message="Simple server is running")
+    return response
+
+@app.route("/create", methods=["POST" , "OPTIONS"])
+@cross_origin()
 def getInfo():
-    textToTranslate = request.json['text']
-    textTranslated = translation(textToTranslate)
-    if not request.json or not 'text' in request.json:
-        abort(400)
+    data = json.loads(request.data)
+    print(data)
+
+    # textToTranslate = request.json['text']
+    # textTranslated = translation(textToTranslate)
+    # if not request.json or not 'text' in request.json:
+    #     abort(400)
+
+       
     return textTranslated    
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host="localhost", port=5001, debug=True)
